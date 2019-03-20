5c0aab631550f11252d02c6afcf8530df93099c2
Reggie <terareggie@hotmail.com>
2019-03-20 10:49:30 -0400

    premier commit

---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Reginald Guerrier on 2019-03-20.
+//  Copyright © 2019 Dygitar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
